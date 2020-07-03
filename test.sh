955a4f63db0f8a2e4f64b2165972c4d23b497847
kkm <kubasurdej@gmail.com>
2020-07-03 20:36:57 +0200

    Add bash function for creating command for running lego

--- test.sh
+++ test.sh
@@ -1,1 +1,13 @@
-./lego --server=https://acme-staging-v02.api.letsencrypt.org/directory --email="foo@bar.com" --domains="example.com" --dns="hyperone" --dns.resolvers="ns-01.hyperone-dns.com" --accept-tos run
+runLegoForDomain() {
+    local ADDITIONAL_PARAMS=$1
+
+    echo ./lego \
+    --server=https://acme-staging-v02.api.letsencrypt.org/directory \
+    --email="foo@bar.com" \
+    --domains="example.com" \
+    --dns.resolvers="ns-01.hyperone-dns.com" \
+    --accept-tos \
+    $ADDITIONAL_PARAMS \
+    run
+}
+
